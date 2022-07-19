458fc892a4945bddade5ce1b8a8d66d2c4d3d422
Shahaf Edri <edri.sue@gmail.com>
2022-07-20 02:27:48 +0300

    add logger to project

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,17 +1,35 @@
-from logging import WARNING, Formatter, basicConfig, getLogger, INFO, FileHandler, StreamHandler
+import logging
+import sys
+from datetime import datetime
 
+logger = logging.getLogger('prk-logger')
+logger.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+ch = logging.StreamHandler(stream=sys.stdout)
+ch.setLevel(logging.INFO)
+# create formatter
+formatter = logging.Formatter('[%(asctime)s] %(levelname)-9s: %(filename)s - %(message)s'.ljust(40))
+# add formatter to ch
+ch.setFormatter(formatter)
+# add ch to logger
+logger.addHandler(ch)
+# Add file handler + UUID for PD runs
+timestamp = str(datetime.now()).replace(" ", "_")
+timestamp = str(datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M_%S'))
+fileHandler = logging.FileHandler(f"{logger.name}__{timestamp}.log", mode="w")
+fileHandler.set_name("file-handler")
+fileHandler.setFormatter(formatter)
+fileHandler.setLevel(logging.DEBUG)
+logger.addHandler(fileHandler)
+# Don't allow root loggers to propagate so logging is not multiplied
+logger.propagate = False
+logger.info(f"INTRO: Will save log to: <{fileHandler.baseFilename}>")
 
-FORMAT = '[%(asctime)s] %(levelname)s in %(filename)s: %(message)s'
-# file handler
-file_handler = FileHandler('server_REST.log', mode='a',)
-file_handler.setLevel(WARNING)
-file_handler.setFormatter(Formatter(FORMAT))
-# stream handler
-stream_handler = StreamHandler()
-stream_handler.setLevel(INFO)
-stream_handler.setFormatter(Formatter(FORMAT))
-# logger
-logger = getLogger()
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
+logger.info("INTRO: Logger initialized")
 
+if __name__ == "__main__":
+    logger.info("INTRO: This is a test")
+    logger.debug("INTRO: This is a test")
+    logger.warning("INTRO: This is a test")
+    logger.error("INTRO: This is a test")
+    logger.critical("INTRO: This is a test")
